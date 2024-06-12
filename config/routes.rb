f640a6b51fa97ef70657c86904cff049420c9e24
dylansarikas <dylan.sarikas@gmail.com>
2024-06-12 15:25:28 -0500

    adds director destroy

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get("/directors/:path_id", { :controller => "directors", :action => "show" })
   post "/directors" => "directors#create"
   post "/directors/:id" => "directors#update"
-  delete "/directors/:id" => "directors#destroy"
+  get "/delete_directors/:id" => "directors#destroy"
 
   get("/movies", { :controller => "movies", :action => "index" })
   get("/movies/:path_id", { :controller => "movies", :action => "show" })
